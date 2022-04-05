8b8f375b9c19efed01740c850e61b636b684f38b
enkhee-Osiris <enkhee.ag@gmail.com>
2022-04-05 08:26:54 +0000

    Update github page

---
+++ _next/static/nz_vkCF3XEo49iBUZPVFA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-2b08e5a62b3fd25e.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
