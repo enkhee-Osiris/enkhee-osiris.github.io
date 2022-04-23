39f9a0a7211e811f8d74445bccf3892920722514
enkhee-Osiris <enkhee.ag@gmail.com>
2022-04-23 16:38:03 +0800

    remove nav menu

--- packages/ui/Header.tsx
+++ packages/ui/Header.tsx
@@ -1,9 +1,51 @@
+import { useCallback } from "react";
+
+import { useKBar } from "kbar";
+import { useDarkMode } from "usehooks-ts";
+
 import { Container } from "./Container";
 import { Flex } from "./Flex";
-import { Grid } from "./Grid";
-import { Nav } from "./Nav";
+import { Icon } from "./Icon";
+import { Row } from "./Row";
+import { styled } from "./stitches.config";
 import { Text } from "./Text";
-import { ThemeToggler } from "./ThemeToggler";
+
+const StyledButton = styled("button", {
+  backgroundColor: "transparent",
+  border: "none",
+  p: 0,
+});
+
+function CommandBarToggler() {
+  const { query } = useKBar();
+
+  const handleClick = useCallback(() => {
+    query.toggle();
+  }, [query]);
+
+  return (
+    <StyledButton type="button" onClick={handleClick}>
+      <Icon css={{ color: "$gray12", cursor: "pointer" }} name="app-folder" />
+    </StyledButton>
+  );
+}
+
+function ThemeToggler() {
+  const { toggle } = useDarkMode(true);
+
+  return (
+    <StyledButton type="button" onClick={toggle}>
+      <Icon
+        css={{ ".dark-theme &": { display: "none" }, color: "$gray10", cursor: "pointer" }}
+        name="moon"
+      />
+      <Icon
+        css={{ ".light-theme &": { display: "none" }, color: "$orange10", cursor: "pointer" }}
+        name="sun"
+      />
+    </StyledButton>
+  );
+}
 
 export type HeaderProps = {
   heading: string;
@@ -12,29 +54,29 @@ export type HeaderProps = {
 export function Header({ heading }: HeaderProps) {
   return (
     <Container as="header" css={{ mb: "$space$84" }}>
-      <Grid columns={{ "@initial": 1, "@md": 2 }} align="center" justify="between" gap={32}>
-        <Nav />
-
-        <Flex
-          direction={{ "@initial": "row", "@md": "columnReverse" }}
-          align={{ "@initial": "start", "@md": "end" }}
-          justify={{ "@initial": "between" }}
+      <Flex
+        direction={{ "@initial": "row", "@md": "columnReverse" }}
+        justify="between"
+        align={{ "@initial": "center", "@md": "start" }}
+      >
+        <Text
+          variant="display"
+          size="sm"
+          weight="bold"
+          css={{
+            marginRight: "$space$8",
+            "@md": { marginTop: "$space$8", marginRight: 0 },
+          }}
         >
-          <Text
-            variant="display"
-            size="sm"
-            weight="bold"
-            css={{
-              marginRight: "$space$8",
-              "@md": { marginTop: "$space$8", marginRight: 0, textAlign: "right" },
-            }}
-          >
-            {heading}
-          </Text>
+          {heading}
+        </Text>
+
+        <Row css={{ "@md": { alignSelf: "end" } }} gap={8}>
+          <CommandBarToggler />
 
           <ThemeToggler />
-        </Flex>
-      </Grid>
+        </Row>
+      </Flex>
     </Container>
   );
 }
