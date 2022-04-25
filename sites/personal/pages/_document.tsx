20ec277e41fb4297c403f44936ca8e79b1401e83
enkhee-Osiris <enkhee.ag@gmail.com>
2022-04-25 22:59:44 +0800

    add css generator function

--- sites/personal/pages/_document.tsx
+++ sites/personal/pages/_document.tsx
@@ -1,10 +1,12 @@
-import { darkTheme } from "@enkhee-Osiris/ui";
+import { darkTheme, getCssText } from "@enkhee-Osiris/ui";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* eslint-disable-next-line react/no-danger */}
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600&display=swap"
           rel="stylesheet"
